0667b58f2a44a277bddde7d5263b93b24b485411
mohannaddQA <qa.mohannad@gmail.com>
2023-05-04 02:14:25 +0000

    added two more routes

--- server.js
+++ server.js
@@ -68,6 +68,38 @@ async function searchPage(request, response) {
 }
 // searchInput
 app.get("/search", searchPage);
+////////////////////////////////////////////////////////genres page
+function Genres(name) {
+  this.name = name;
+}
+async function genresPage(request, response) {
+  const genresApiURL = `https://api.themoviedb.org/3/genre/movie/list?api_key=${ApiKey}`;
+  const movieData = await axios.get(genresApiURL);
+  let genres = movieData.data.genres.map((item) => {
+    return new Genres(item.name);
+  });
+  response.send(genres);
+}
+
+app.get("/genres", genresPage);
+////////////////////////////////////////////////////////providers
+
+function Providers(id, name, logo) {
+  this.provider_id = id;
+  this.provider_name = name;
+  this.logo_path = logo;
+}
+async function regionalProviders(request, response) {
+  const providersApiURL = `https://api.themoviedb.org/3/watch/providers/movie?api_key=${ApiKey}`;
+  const movieData = await axios.get(providersApiURL);
+  let providers = movieData.data.results.map((item) => {
+    return new Providers(item.provider_id, item.provider_name, item.logo_path);
+  });
+  response.send(providers);
+}
+
+app.get("/providers", regionalProviders);
+
 ////////////////////////////////////////////////////////error handlers
 app.use("*", (req, res) => {
   let errorMassage = `error status : 404    responseText:Sorry, Page not found`;
